3ab986a7ade4b53385e717b4c581e1a23101ce48
walter kato <walter.kato@despegar.com>
2018-09-07 19:22:33 -0300

    tp 2

---
+++ tallerJavaTp2/src/ttps/clasificados/Login.java
@@ -0,0 +1,57 @@
+package ttps.clasificados;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "ttps.clasificados.Login",urlPatterns = {"/login"})
+
+public class Login extends HttpServlet {
+    Map<String,String> users;
+    Map<String,String> usersRol;
+
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        users = new HashMap<>();
+        usersRol = new HashMap<>();
+        users.put("ezequiel@gmail.com","123");
+        users.put("andres@gmail.com","123");
+        users.put("kato@gmail.com","123");
+        users.put("franco@gmail.com","123");
+        usersRol.put("ezequiel@gmail.com","publicador");
+        usersRol.put("andres@gmail.com","publicador");
+        usersRol.put("kato@gmail.com","admin");
+        usersRol.put("franco@gmail.com","admin");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String mail = request.getParameter("email");
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/menu");
+        if(users.containsKey(request.getParameter("email"))){
+            if(request.getParameter("password").equals(users.get(request.getParameter("email")))){
+
+                request.setAttribute("rol",usersRol.get(request.getParameter("email")));
+                dispatcher.forward(request, response);
+
+
+
+            }else{
+                response.sendRedirect("error.html");
+            }
+        }
+        else response.sendRedirect("error.html");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("login.html");
+    }
+}
---
+++ tallerJavaTp2/src/ttps/clasificados/User.java
@@ -0,0 +1,24 @@
+package ttps.clasificados;
+
+public class User {
+
+    private String email;
+    private String clave;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+}
